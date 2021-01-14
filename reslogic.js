8337254d4c5435b111a364b18ab9ff40ea5038d9
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-01-13 20:36:27 -0500

    General updates to reservations html file and css. Added pseudo coding reslogic file.

--- reslogic.js
+++ reslogic.js
@@ -1,8 +1,29 @@
 
+
+
+// maybe using this variable for directions
 // var information = {
 //    email: "stored email"
 //    phone: "stored phone"
 // }
+
+// button on totals page
+
+//$(".button-total").on("click"m function(){
+    // var buttoncost = $("clicktotalcost").val();
+    // maybe use local storage
+    // maybe pull from one variable on page
+    // call function findCurrency(valueofcurrency);
+
+
+///});
+
+// value for currency selected
+// drop down menu
+// then ok based on drop down
+
+
+// value for call //
 
 $(".button-email").on("click", function(){ 
 
@@ -18,4 +39,44 @@ $(".button-phone").on("click", function(){
     $("#emailtext").val(localStorage.setItem("Phone", JSON.stringify(phoneText)))
 
 });
+
+// Using Currency Calculator
+
+// function findCurrency(valueofcurrency){
+
+   // endpoint: 'convert' ;
+   // access_key:         ;
+
+   // define currencies
+
+   // from = 'currency value selected' 
+   // to = "specified currency"
+   // amount = 'to' // Car.total = $(".placewheretotaliscalculated")
+
+   // var convertValue = 'https://api.currencylayer.com/' + endpoint + '?access_key=' + access_key +'&from=' + from + '&to=' + to + '&amount=' + amount, 
+
+   // $.ajax({
+       // url: convertValue,
+       // method: "GET",
+   // }).then(function(response){
+       // console.log(convertValue);
+       // console.log(response);
+
+       // call out variables based on console object
+       //
+
+       // from documentation
+       // $.ajax({
+           // url:
+           // datatype: 'jsonp'
+           // success: function(finalValue){
+               //alert(finalValue.result);
+
+           //}
+
+       //})
+
+
+   //})
+
 
